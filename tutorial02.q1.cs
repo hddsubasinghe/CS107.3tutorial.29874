52a1906d0e8a05b01bab5662c6cb5ebc0b2649d9
hddsubasinghe <136324413+hddsubasinghe@users.noreply.github.com>
2024-02-13 09:14:07 +0530

    Add files via upload

---
+++ tutorial02.q1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02q1
+{
+    internal class Book
+    {
+   
+            public string Title { get; set; }
+            public string Author { get; set; }
+        }
+
+        class program
+        {
+            static void Main()
+            {
+               
+                Book myBook = new Book();
+
+             
+                myBook.Title = "The Great Gatsby";
+                myBook.Author = "F. Scott Fitzgerald";
+
+                
+                Console.WriteLine("Book Information:");
+                Console.WriteLine($"Title: {myBook.Title}");
+                Console.WriteLine($"Author: {myBook.Author}");
+
+                
+                Console.ReadLine();
+            }
+        }
+
+    }
+
+
