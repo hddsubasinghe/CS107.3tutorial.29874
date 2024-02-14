bf101751a3c924f0afd8b4f5f5bbdbbe72fb73db
hddsubasinghe <136324413+hddsubasinghe@users.noreply.github.com>
2024-02-14 09:05:47 +0530

    Add files via upload

---
+++ Tutorial02.q4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial02q4
+{
+    
+    
+        class Product
+        {
+            
+            public string ProductName { get; set; }
+            public double Price { get; set; }
+
+            public Product(string productName, double price)
+            {
+                ProductName = productName;
+                Price = price;
+            }
+
+            
+            public void DisplayProductDetails()
+            {
+                Console.WriteLine($"Product: {ProductName}");
+                Console.WriteLine($"Price: ${Price:F2}\n");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                
+                Product product1 = new Product("Laptop", 899.99);
+                Product product2 = new Product("Smartphone", 499.99);
+
+               
+                Console.WriteLine("Product Details:");
+                product1.DisplayProductDetails();
+                product2.DisplayProductDetails();
+            }
+        }
+    }
+
