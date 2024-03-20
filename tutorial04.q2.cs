53f4c8c9842205f5ce1bcd10dda7c0ffbe56684a
hddsubasinghe <136324413+hddsubasinghe@users.noreply.github.com>
2024-03-20 09:05:25 +0530

    Add files via upload

---
+++ tutorial04.q2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab04q2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(textBox1.Text);
+                int num2 = int.Parse(textBox2.Text);
+                string operation = comboBox1.SelectedItem.ToString();
+
+                int result = 0;
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "x":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                            MessageBox.Show("Cannot divide by zero.");
+                        break;
+                }
+
+                txtResult.Text = "Result: " + result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid integers.");
+            }
+        }
+    }
+}
